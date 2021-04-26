df7d98418a1d881ba60ff53992acafa6e187ab2a
infyshine <infyshine@DESKTOP-02>
2021-04-26 13:13:49 +0530

    12

--- src/com/infyshine/StringTokenizerTest.java
+++ src/com/infyshine/StringTokenizerTest.java
@@ -6,9 +6,9 @@ public class StringTokenizerTest {
 
 	public static void main(String[] args) {
 	   StringTokenizer st=
-			   new StringTokenizer("Sravani sravs");
-	           new StringTokenizer("Sravani sravs","a");
-	           new StringTokenizer("Sravani sravs","a",true);
+			   new StringTokenizer("Sravani s");
+	           new StringTokenizer("Sravani s","a");
+	           new StringTokenizer("Sravani s","a",true);
 	   System.out.println("Number of Tokens:"+st.countTokens());
 	   while(st.hasMoreTokens()) {
 		   String token=st.nextToken();
